8ae4b1e78af75e162119465379eda7ba9317ed2f
Elton de Freitas <eltondefreitas@outlook.com>
2023-10-29 13:18:51 -0300

    Inclusão de novo exemplo

--- pyles1_strconc.py
+++ pyles1_strconc.py
@@ -4,6 +4,8 @@ pronome = "Excelentíssimo"
 name = "Fulano" # some string variable
 surname = "de Tal"
 
+lista = ['Fulano','Ciclano','Beltrano','João']
+
 print("\nSeu nome é "+ name +" e seu sobrenome é " + surname)
 
 print("\nSeu nome é",name,"e seu sobrenome é",surname)
@@ -20,3 +22,11 @@ direct_mail =  f"\n {pronome} senhor(a) {name}, \ncomo tem passado? \
     \nGostariamos muito que você revissase seus dados de nome \"{name}\" e sobrenome \"{surname}\" para ter o dado correto em nossa base \
         "
 print(direct_mail)
+
+# from this point on we will simulate a simple mail merge using a list of names
+# where we will print a standard message changing between the names in the list
+
+for nome in lista:
+    print(f"\n\nOlá {pronome} senhor(a)",nome,"!!!\n \
+        Gostariamos de convida-lo para nossa Festa. \
+            Gostamos muito de você",nome," e contamos com sua presença!")
